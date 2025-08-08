d8ff736184674e7217703c37623b903ca6bc020c
Kenzkyi <ekeneokoye93@gmail.com>
2025-08-08 09:19:25 +0100

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,21 @@ import { useEffect } from "react";
 const App = () => {
   const nominatimUrl = "https://nominatim.openstreetmap.org/reverse";
   const userAgent = "divest-admin-dashboard/2.0";
+
+  const onSucces = (arg0: GeolocationPosition) => {
+    console.log("res", arg0.coords);
+  };
+
+  const onError = (arg0: GeolocationPositionError) => {
+    alert(arg0.message);
+    console.log("error", arg0.message);
+  };
   const locationSetter = async () => {
+    navigator.geolocation.getCurrentPosition(onSucces, onError, {
+      enableHighAccuracy: true,
+      timeout: 5000,
+      maximumAge: 30,
+    });
     try {
       const params = {
         format: "jsonv2",
